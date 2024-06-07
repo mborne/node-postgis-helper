64b8067af2c45866a82f048d5af84014fb1fcf99
mborne <mborne@users.noreply.github.com>
2024-06-08 00:57:18 +0200

    fix(ssl): improve PGSSLMODE handling

--- src/internal/pool.js
+++ src/internal/pool.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg');
 
-const poolConfig = {
-    ssl: {
-        rejectUnauthorized: false,
-    }
-};
+const poolConfig = {};
 
+/**
+ * Try to handle PGSSLMODE :
+ *
+ * - disable|allow|prefer -> disabled with NodeJS
+ * - require -> enabled
+ * - verify-ca and verify-full -> not supported for now
+ * 
+ * @see https://docs.postgresql.fr/9.6/libpq.html#libpq-connect-sslmode
+ */
+const sslMode = process.env.PGSSLMODE || null;
+if (sslMode != null && !['disable', 'allow', 'prefer'].includes(sslMode)) {
+    poolConfig.ssl = {
+        rejectUnauthorized: false
+    }
+}
 
 const pool = new Pool(poolConfig);
 
